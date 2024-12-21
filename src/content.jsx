d157f5761ccaa5e7ff15a3ea69f0491d8f6b943a
tatsuya-asami <sharekobekko@gmail.com>
2024-12-21 12:35:18 +0900

    add manifest configuration and content script; refactor App component for consistency

---
+++ src/content.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+const root = document.createElement("div");
+root.id = "save-your-type-save-your-time";
+document.body.appendChild(root);
+
+ReactDOM.createRoot(root).render(<StrictMode></StrictMode>);
