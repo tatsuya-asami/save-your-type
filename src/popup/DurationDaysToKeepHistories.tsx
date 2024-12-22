ab99ab7bac232b959f649921956d77934043a660
tatsuya-asami <sharekobekko@gmail.com>
2024-12-22 16:38:58 +0900

    refactor DurationDaysToKeepHistories component to use Stack for improved layout and styling

--- src/popup/DurationDaysToKeepHistories.tsx
+++ src/popup/DurationDaysToKeepHistories.tsx
@@ -3,29 +3,33 @@ import HistoryIcon from "@mui/icons-material/History";
 import TextField from "@mui/material/TextField";
 import { ListItem } from "./ListItem";
 import { useChromeStorageSettings } from "../store/useChromeStorageSettings";
+import { Stack } from "@mui/material";
 
 export const DurationDaysToKeepHistories: React.FC = () => {
   const { settings, updateSettings } = useChromeStorageSettings();
 
   return (
     <ListItem icon={<HistoryIcon />} handleClick={() => {}}>
-      <ListItemText
-        primary={"Duration to keep histories"}
-        secondary={
-          <TextField
-            variant="standard"
-            label="days"
-            type="number"
-            value={settings.durationDaysToKeepHistories}
-            onChange={(e) =>
-              updateSettings(
-                "durationDaysToKeepHistories",
-                Number(e.target.value)
-              )
-            }
-          />
-        }
-      />
+      <Stack>
+        <ListItemText primary={"Duration to keep histories"} />
+        <ListItemText
+          primary={
+            <TextField
+              size="small"
+              variant="standard"
+              label="days"
+              type="number"
+              value={settings.durationDaysToKeepHistories}
+              onChange={(e) =>
+                updateSettings(
+                  "durationDaysToKeepHistories",
+                  Number(e.target.value)
+                )
+              }
+            />
+          }
+        />
+      </Stack>
     </ListItem>
   );
 };
