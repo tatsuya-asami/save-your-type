bcd0a92ef94be1934661306159b284dd610fcce9
tatsuya-asami <sharekobekko@gmail.com>
2024-12-22 14:48:25 +0900

    add delete history option to Settings component for enhanced user control

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -1,5 +1,26 @@
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import Box from "@mui/material/Box";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const Settings: React.FC = () => {
-  return <Box>Settings</Box>;
+  return (
+    <Box>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <DeleteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Delete all histories" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 };
