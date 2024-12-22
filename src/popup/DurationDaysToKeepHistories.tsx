05c102f390a2f49648b0ea8dd1a39118fc44ed6f
tatsuya-asami <sharekobekko@gmail.com>
2024-12-22 18:24:49 +0900

    refactor DurationDaysToKeepHistories component to use ListItemInputNumber for improved code organization and reusability

--- src/popup/DurationDaysToKeepHistories.tsx
+++ src/popup/DurationDaysToKeepHistories.tsx
@@ -1,37 +1,22 @@
-import ListItemText from "@mui/material/ListItemText";
 import HistoryIcon from "@mui/icons-material/History";
-import TextField from "@mui/material/TextField";
-import { ListItem } from "./ListItem";
 import { useChromeStorageSettings } from "../store/useChromeStorageSettings";
-import { Stack } from "@mui/material";
+import { ListItemInputNumber } from "./ListItemInputNumber";
 
 export const DurationDaysToKeepHistories: React.FC = () => {
   const { settings, updateSettings } = useChromeStorageSettings();
 
   return (
-    <ListItem icon={<HistoryIcon />} handleClick={() => {}}>
-      <Stack alignItems={"center"} direction={"row"} spacing={2}>
-        <ListItemText primary={"Duration to keep histories"} />
-        <ListItemText
-          primary={
-            <TextField
-              slotProps={{
-                htmlInput: { min: 1, max: 180, style: { width: 60 } },
-              }}
-              variant="standard"
-              label="days"
-              type="number"
-              value={settings.durationDaysToKeepHistories}
-              onChange={(e) =>
-                updateSettings(
-                  "durationDaysToKeepHistories",
-                  Number(e.target.value)
-                )
-              }
-            />
-          }
-        />
-      </Stack>
-    </ListItem>
+    <ListItemInputNumber
+      icon={<HistoryIcon />}
+      handleClick={() => {}}
+      label={"days"}
+      value={settings.durationDaysToKeepHistories}
+      slotProps={{
+        htmlInput: { min: 1, max: 180, style: { width: 60 } },
+      }}
+      handleChange={(e) =>
+        updateSettings("durationDaysToKeepHistories", Number(e.target.value))
+      }
+    />
   );
 };
---
+++ src/popup/ListItemInputNumber.tsx
@@ -0,0 +1,43 @@
+import ListItemText from "@mui/material/ListItemText";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import { ListItem } from "./ListItem";
+import { Stack } from "@mui/material";
+import { ReactNode } from "react";
+
+type Props = {
+  icon: ReactNode;
+  handleClick: () => void;
+  label: string;
+  value: number;
+  slotProps?: TextFieldProps["slotProps"];
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const ListItemInputNumber: React.FC<Props> = ({
+  icon,
+  handleClick,
+  value,
+  label,
+  slotProps,
+  handleChange,
+}) => {
+  return (
+    <ListItem icon={icon} handleClick={handleClick}>
+      <Stack alignItems={"center"} direction={"row"} spacing={2}>
+        <ListItemText primary={"Duration to keep histories"} />
+        <ListItemText
+          primary={
+            <TextField
+              slotProps={slotProps}
+              variant="standard"
+              label={label}
+              type="number"
+              value={value}
+              onChange={handleChange}
+            />
+          }
+        />
+      </Stack>
+    </ListItem>
+  );
+};
