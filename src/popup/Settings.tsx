c2750d3ea0e6d2950a905c0973898f7aa720b108
tatsuya-asami <sharekobekko@gmail.com>
2024-12-22 15:31:57 +0900

    comment out UrlBlackList component in Settings for future reference

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -1,14 +1,14 @@
 import List from "@mui/material/List";
 import Box from "@mui/material/Box";
 import { DeleteAllHistories } from "./DeleteAllHistories";
-import { UrlBlackList } from "./UrlBlackList";
+// import { UrlBlackList } from "./UrlBlackList";
 
 export const Settings: React.FC = () => {
   return (
     <Box width={"max-content"}>
       <List>
         <DeleteAllHistories />
-        <UrlBlackList />
+        {/* <UrlBlackList /> */}
       </List>
     </Box>
   );
