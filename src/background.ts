313a821b2c85424e66f9bd22bd9b2aaf6bc0e38c
tatsuya-asami <sharekobekko@gmail.com>
2024-12-31 12:35:23 +0900

    feat: implement retry logic for setting storage and handle storage limit exceeded

--- src/background.ts
+++ src/background.ts
@@ -8,13 +8,32 @@ const HISTORIES_KEY = "save-your-type";
 
 chrome.runtime.onMessage.addListener(
   async (message: { type: string; value: Store }) => {
-    switch (message.type) {
+    const { type, value: newValue } = message;
+    switch (type) {
       case HISTORIES_KEY: {
-        try {
-          const histories = await getHistories();
-          await setHistories([...histories, message.value]);
-        } catch (error) {
-          console.error("Error accessing storage:", error);
+        let histories = await getHistories();
+        let value = histories ? [...histories, newValue] : [newValue];
+        let attempts = 0;
+
+        while (attempts < 5) {
+          try {
+            await setHistories(value);
+            return;
+          } catch (error) {
+            if (!(error instanceof Error)) {
+              return;
+            }
+            if (error.message === "Storage limit exceeded") {
+              console.warn("Storage limit exceeded. Removing oldest values...");
+              await removeOldHistories();
+              histories = await getHistories();
+              value = histories ? [...histories, newValue] : [newValue];
+            } else {
+              console.error("Failed to set storage:", error);
+              return;
+            }
+          }
+          attempts++;
         }
 
         break;
@@ -52,4 +71,15 @@ const setHistories = (value: Store[]) => {
       }
     });
   });
+};
+
+const removeOldHistories = async () => {
+  const histories = await getHistories();
+  if (!histories || histories.length === 0) {
+    return;
+  }
+  histories.shift();
+  histories.shift();
+  histories.shift();
+  await setHistories(histories);
 };
