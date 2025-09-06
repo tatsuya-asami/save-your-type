4536144fbda059da13b4a3f1d69769f6269f4c00
tatsuya-asami <sharekobekko@gmail.com>
2025-09-06 18:19:14 +0900

    feat: implement dark mode theme support in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import "./App.css";
 import { Root } from "./popup/Root";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const isDarkMode = window.matchMedia?.("(prefers-color-scheme: dark)").matches;
+
+const darkTheme = createTheme({
+  palette: {
+    mode: isDarkMode ? "dark" : "light",
+  },
+});
 
 function App() {
-  return <Root />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Root />
+    </ThemeProvider>
+  );
 }
 
 export default App;
